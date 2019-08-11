e04bb9034c6a6f078733910621be207f1c580290
David Nolan <davidnolan458@gmail.com>
2019-08-11 17:48:35 +0100

    commit

--- RepeatprojectCbingo/RepeatprojectCbingo/Source.c
+++ RepeatprojectCbingo/RepeatprojectCbingo/Source.c
@@ -1,11 +1,11 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <time.h>
 
 
-//#define ROW 3
-//#define COL 9
+#define ROW 3
+#define COL 9
 #define MAX_NUM_PLAYERS 6
 
 typedef struct card_slot {
@@ -14,10 +14,11 @@ typedef struct card_slot {
 } card_slot;
 void main()
 {
-	int i, j, n;
+	int i, j;
 	//int BINGO_CARD[ROW][COL];
 	int Numofplayers;
 	int bingo_card[9][3];
+	int p = 0;
 
 
 
@@ -27,16 +28,42 @@ void main()
 	scanf_s("%d", &Numofplayers);
 	//TEST
 	//printf("%d", Numofplayers);
-	while (Numofplayers != MAX_NUM_PLAYERS)
+	
+	//Input check
+	while (Numofplayers < 1 || Numofplayers > MAX_NUM_PLAYERS)
 	{
+		printf("Invalid Input!");
+		printf("How many players are there playing?\n");
+		scanf("%d", &Numofplayers);
+	}
+		
+		//bingo card generation
+		
+	//while (p!=Numofplayers) {
 		for (i = 0; i < 9; i++)
 		{
 			for (j = 0; j < 3; j++)
 			{
-				bingo_card[i][j] = rand() % 50;
-				printf("%d %d\n", bingo_card[i][j]);
-			}
-		}//EO For
+				bingo_card[i][j] = rand() % 8 + 1;
+				printf("%d\t", bingo_card[i][j]);
+				bingo_card[i][j] = 20 + rand() % 9;
+				printf("%d\t", bingo_card[i][j]);
+				bingo_card[i][j] = 30 + rand() % 9;
+				printf("%d\t", bingo_card[i][j]);
+				bingo_card[i][j] = 40 + rand() % 9;
+				printf("%d\t", bingo_card[i][j]);
+				bingo_card[i][j] = 50 + rand() % 9;
+				printf("%d\t", bingo_card[i][j]);
+				bingo_card[i][j] = 60 + rand() % 9;
+				printf("%d\t", bingo_card[i][j]);
+				bingo_card[i][j] = 70 + rand() % 9;
+				printf("%d\t", bingo_card[i][j]);
+				bingo_card[i][j] = 80 + rand() % 9;
+				printf("%d\t", bingo_card[i][j]);
 
-	}//EO while
+			}//EO for
+		}//EO For
+	//	p++;
+	//}//EO While
+	
 }//end of main
